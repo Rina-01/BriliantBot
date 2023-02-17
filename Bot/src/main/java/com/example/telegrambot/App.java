7ddcd56b905b37ac370c0b0a5d6cf4ce6d213069
Rina-01 <ira.mashkova.2001@mail.ru>
2023-02-17 10:49:36 +0300

    upd mes 0.1
    trying update message received from user

--- Bot/src/main/java/com/example/telegrambot/App.java
+++ Bot/src/main/java/com/example/telegrambot/App.java
@@ -8,9 +8,8 @@ import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 public class App {
     public static void main(String[] args) {
         try {
-            TelegramBotsApi botsApi =
-                    new TelegramBotsApi(
-                            DefaultBotSession.class);
+            TelegramBotsApi botsApi = new TelegramBotsApi(
+                    DefaultBotSession.class);
             botsApi.registerBot(new Bot());
             System.out.println("YES");
         } catch (TelegramApiException e) {
--- Bot/src/main/java/com/example/telegrambot/bot/Bot.java
+++ Bot/src/main/java/com/example/telegrambot/bot/Bot.java
@@ -6,12 +6,6 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class Bot extends TelegramLongPollingBot {
-
-//    @Override
-//    public void onUpdateReceived(Update update) {
-//
-//    }
-
     @Override
     public String getBotUsername() {
         return "HelpToPlanStudyBot";
@@ -28,7 +22,11 @@ public class Bot extends TelegramLongPollingBot {
         if (update.hasMessage() && update.getMessage().hasText()) {
             SendMessage message = new SendMessage(); // Create a SendMessage object with mandatory fields
             message.setChatId(update.getMessage().getChatId().toString());
-            message.setText(update.getMessage().getText());
+
+            String UserText = update.getMessage().getText();
+            UserText += ":))";
+
+            message.setText(UserText);
 
             try {
                 execute(message); // Call method to send the message
